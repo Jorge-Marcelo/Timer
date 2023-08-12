d30f9bc4e56386e51525f7608c00f3fc5fa40e0f
Jorge Marcelo <49494259+Jorge-Marcelo@users.noreply.github.com>
2023-08-12 16:52:06 -0300

    Adicionando comentarios no Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+/*
+Autor: Jorge Marcelo
+Data de Criação: 12/08/2023
+Github: https://github.com/Jorge-Marcelo
+Linkedin: https://www.linkedin.com/in/jorge-marcelo-067a5017b/
+Instagram: https://www.instagram.com/10_stronger/
+Threads: https://www.threads.net/@10_stronger
+*/ 
 
+/*Pegando  os elementos pelo DOM*/
 let spanHoras = document.getElementById("horas");
 let spanMinutos = document.getElementById("minutos");
 let spanSegundos = document.getElementById("segundos");
@@ -6,22 +15,28 @@ let playButton = document.getElementById("play");
 let pauseButton = document.getElementById("pause");
 let retryButton = document.getElementById("retry");
 
+/*Inicializando variaveis*/
 let timer;
 let horas = 0;
 let minutos = 0;
 let segundos = 0;
 
-
+/*Função de iniciar o contador*/
 function play(evento) {
 evento.preventDefault();
+  /*setInterval para o contador contar de 1 em 1 segundo*/
 timer = setInterval(atualizar, 1000);
 }
 
+/*unção para pausar*/
 function pause(evento) {
 evento.preventDefault();
 clearInterval(timer);
 }
 
+/*Função para reiniar
+OBS: tive que colocar -1 no segundos 
+pq a variavel de segundos ja inicia incrementando*/
 function retry(evento) {
 evento.preventDefault();
 clearInterval(timer);
@@ -31,6 +46,8 @@ segundos = - 1;
 atualizar();
 }
 
+/*Função para atualizar
+os numeros do contador*/ 
 function atualizar() {
 segundos++;
 if (segundos === 60) {
@@ -43,11 +60,13 @@ minutos = 0;
 horas++;
 }
 
+ /*Adicionando zero a esquerda aos valores*/
 spanHoras.textContent = zeroEsquerda(horas);
 spanMinutos.textContent = zeroEsquerda(minutos);
 spanSegundos.textContent = zeroEsquerda(segundos);
 }
 
+/*Função de adicionar zero a esquerda*/
 function zeroEsquerda(value) {
 if (value < 10) {
 return "0" + value;
@@ -56,6 +75,7 @@ return value;
 }
 }
 
+/*Escutadores de eventos para os botões*/
 playButton.addEventListener("click", play);
 pauseButton.addEventListener("click", pause);
 retryButton.addEventListener("click", retry);
